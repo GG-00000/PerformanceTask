9c5acda834a13c1eeec65e2af76e6fd059dd584a
GG-00000 <ggutierrez48@cps.edu>
2026-02-26 20:29:50 +0000

    Final

--- performance_task/performance_task.py
+++ performance_task/performance_task.py
@@ -1,49 +1,62 @@
-meals = ["McChiken","Big Mac","QuaterPounder"],
-sides = ["Small Fries", "Medium Fries", "Large Fries"],
+
+
+meals = ["McChiken", "Big Mac", "QuarterPounder"]
+meal_prices = [3.50, 4.00, 4.25]
+sides = ["Small Fries", "Medium Fries", "Large Fries"]
+side_prices = [2.15, 2.50, 2.75]
 drinks = ["Coke", "Pepsi", "Sprite"]
-    
-def find_meal(order):
-    for meal in meals:
-        if meal[0] == order:
-            return meal
+drink_prices = [1.50, 1.25, 1.35]
+
+
+def find_item(order, item_list):
+    for item in item_list:
+        if item.lower() == order.lower():
+            return item
     return None
 
-def find_side(order):
-    for side in sides:
-        if side[0] == order:
-            return side
-    return None
+def calculate_total(meal, side, drink):
+    meal_index = meals.index(meal)
+    side_index = sides.index(side)
+    drink_index = drinks.index(drink)
 
-def find_drink(order):
-    for drink in drinks:
-        if drink[0] == order:
-            return drink
-    return None
+    total = (
+        meal_prices[meal_index]
+        + side_prices[side_index]
+        + drink_prices[drink_index]
+    )
+    return total
+
+def tell_order(meal, side, drink):
+    total = calculate_total(meal, side, drink)
+    print("\nHere's your order:")
+    print(meal, "-", meal_prices[meals.index(meal)])
+    print(side, "-", side_prices[sides.index(side)])
+    print(drink, "-", drink_prices[drinks.index(drink)])
+    print("Total: $", total)
     
-def tell_order(meal,side,drink):
-    print("heres your order")
-    print(meal)
-    print(side)
-    print(drink)
-
-Beginning = input("Welcome would you like to order something today?")
-
-while Beginning == "yes":
-    print("Here are our meals: McChiken, Big Mac, QuarterPounder")
-    print("Here are our sides: Small Fries, Medium Fries, Large Fries")
-    print("Here are our drinks: Coke, Pepsi, Sprite")
-    meal = input("what do you want for your meal?")
-    side = input("what do you want for your side?")
-    drink = input("what do you want for your drink?")
-
-    if meal:
-        tell_order(meal[0])
-    else:
-        print("That meal doesnt exist")
-    
-    if side:
-        tell_order(side[0])
-
-        print("Okay goodbye")
-def outro():
-    print("Thanks for ordering from McMarco's")
+def main():
+    beginning = input("Welcome! Would you like to order something today? ")
+    while beginning.lower() == "yes":
+        add_new = input("\nWould you like to add a new meal to the menu? (yes/no) ")
+        if add_new.lower() == "yes":
+            new_meal = input("Enter the name of the new meal: ")
+            new_price = float(input("Enter the price of the new meal: "))
+            meals.append(new_meal) 
+            meal_prices.append(new_price) 
+        print(new_meal, "has been added to the menu!")
+        print("\nHere are our meals:", meals)
+        print("Here are our sides:", sides)
+        print("Here are our drinks:", drinks)
+        meal_input = input("What do you want for your meal? ")
+        side_input = input("What do you want for your side? ")
+        drink_input = input("What do you want for your drink? ")
+        meal = find_item(meal_input, meals)
+        side = find_item(side_input, sides)
+        drink = find_item(drink_input, drinks)
+        if meal and side and drink:
+            tell_order(meal, side, drink)
+        else:
+            print("One or more items don't exist. Please try again.")
+        beginning = input("\nWould you like to order again? (yes/no) ")
+    print("Thanks for ordering from McMarco's!")
+main()
